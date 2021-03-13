3b49aacb23b6b0cc2c6615910ca4d1b183453be6
Luis Filipe Alves de Oliveira <luis.oliveira@macbook-pro-lf-oliveira.box>
2021-03-12 22:38:06 -0300

    feat(app): make layout UI introduction home work screen

---
+++ src/core/ui/components/Button/styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components/native';
+
+export const Button = styled.TouchableOpacity`
+  align-items: center;
+  justify-content: center;
+  border-radius: 14px;
+  background: #00b3f1;
+  height: 44px;
+`;
+
+export const TextButton = styled.Text`
+  color: #ffffff;
+  font-size: 16px;
+`;
---
+++ src/core/ui/styles/colors.ts
@@ -0,0 +1,12 @@
+const colors = {
+  bluePrimary: '#06397D',
+  blueSecondary: '#00B3F1',
+  red: '#FF004A',
+  black: '#161A20',
+  greyDark: '#666666',
+  greyMedium: '#B5B5B5',
+  grayLight: '#F5F5F5',
+  white: '#FFF',
+};
+
+export default colors;
