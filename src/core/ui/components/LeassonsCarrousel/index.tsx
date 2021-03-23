ab4855148a46cf74ccd20e132735d42f3764bba9
Luis Filipe Alves de Oliveira <luis.oliveira@macbook-pro-lf-oliveira.local>
2021-03-22 23:50:07 -0300

    refactor(app): refactor carousel to FlatList

--- src/core/ui/components/LeassonsCarrousel/index.tsx
+++ src/core/ui/components/LeassonsCarrousel/index.tsx
@@ -1,4 +1,5 @@
 import React, { ReactElement } from 'react';
+import { FlatList } from 'react-native';
 
 import { Container, TextLeasson, ContainerText, ItemCarrousel } from './styles';
 
@@ -7,30 +8,32 @@ import { Image, View } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
 
 export default function LeassonsCarrouselComponent({
-    items
+  items,
 }: LeassonsCarrouselProps): ReactElement {
-    return (
-        <ScrollView
-            style={{
-                marginTop: 15,
-                marginBottom: 25,
-                marginLeft: 10
-            }}
-            horizontal={true}
-            showsHorizontalScrollIndicator={false}
-            pagingEnabled={true}
-        >
-            {
-                items.map((leasson: LeassonItem) =>
-                    <ItemCarrousel key={leasson.id}>
-                        <Container onPress={() => console.log('CLICADO NO ITEM' + leasson.id)}>
-                            <Image source={{ uri: leasson.url_image }} style={{ width: 253, height: 162, borderRadius: 8, opacity: leasson.active === true ? 1 : 0.2 }} />
-                            <ContainerText style={{ opacity: leasson.active === true ? 1 : 0.2 }}>
-                                <TextLeasson>{leasson.title}</TextLeasson>
-                            </ContainerText>
-                        </Container>
-                    </ItemCarrousel>)
-            }
-        </ScrollView>
-    );
+  return (
+    <FlatList
+      data={items}
+      keyExtractor={(item) => item.id}
+      renderItem={({ item }) => (
+        <ItemCarrousel>
+          <Container onPress={() => console.log('CLICADO NO ITEM' + item.id)}>
+            <Image
+              source={{ uri: item.url_image }}
+              style={{
+                width: 253,
+                height: 162,
+                borderRadius: 8,
+                opacity: item.active === true ? 1 : 0.2,
+              }}
+            />
+            <ContainerText style={{ opacity: item.active === true ? 1 : 0.2 }}>
+              <TextLeasson>{item.title}</TextLeasson>
+            </ContainerText>
+          </Container>
+        </ItemCarrousel>
+      )}
+      horizontal
+      showsHorizontalScrollIndicator={false}
+    />
+  );
 }
