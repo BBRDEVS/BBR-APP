06f6bc8eb0f373b00fd2b23e6d97f12a05c9789b
Julio Rodrigues <juulhao@gmail.com>
2021-07-04 14:23:59 -0300

    chat pré feito

--- src/core/ui/components/Balloon/BalloonAnswer/BalloonAnswer.tsx
+++ src/core/ui/components/Balloon/BalloonAnswer/BalloonAnswer.tsx
@@ -5,22 +5,23 @@ import { AnswerBalloonProps } from './props';
 
 export default function BalloonAnswer({ title }: AnswerBalloonProps): ReactElement {
     return (
-        <View 
+        <View
+        style={{
+            backgroundColor: colors.bluePrimary,
+            borderRadius: 30,
+            paddingLeft: 25,
+            paddingRight: 25,
+            paddingTop: 17,
+            paddingBottom: 18
+        }}
+    >
+        <Text
             style={{
-                backgroundColor: colors.bluePrimary,
-                borderRadius: 30,
-                paddingLeft: 25,
-                paddingRight: 25,
-                paddingTop: 17,
-                paddingBottom: 18
+                color: colors.white,
+                fontSize: 16
             }}
-        >
-            <Text
-                style={{
-                    color: colors.white,
-                    fontSize: 16
-                }}
-            >{title}</Text>
-        </View>
+        >{title}</Text>
+    </View>
+
     );
-  }
+}
--- src/core/ui/pages/Chat/Chat.tsx
+++ src/core/ui/pages/Chat/Chat.tsx
@@ -1,5 +1,5 @@
-import React, { ReactElement, useEffect, useState } from 'react';
-import { Button, SafeAreaView, Text, TextInput, View } from 'react-native';
+import React, { ReactElement, useCallback, useEffect, useState } from 'react';
+import { Button, InteractionManager, SafeAreaView, Text, TextInput, View } from 'react-native';
 
 import { Container, } from './styles';
 
@@ -17,12 +17,12 @@ import * as Yup from 'yup';
 export interface IPayloadQuestions {
   id: number;
   title: string;
+  response: string;
 }
 
 export default function Chat({ }: ChatProps): ReactElement {
-  const [questionCounter, setQuestionCounter] = useState(0);
-  const [listQuestion, setQuestionList] = useState([]);
-  const [listAnswerStudent, setlistAnswerStudent] = useState([]);
+  const [message, setMessage] = useState([{ id: 0, title: 'Quando se pensa em comida, qual é o primeiro verbo que você pensa?' }]);
+  const [countMessageUser, setCounterMessageUser] = useState(1);
 
   const formik = useFormik({
     initialValues: { message: '' },
@@ -32,82 +32,100 @@ export default function Chat({ }: ChatProps): ReactElement {
     }),
 
     onSubmit: (values) => {
-      setlistAnswerStudent((listAnswerStudent) => [
-        ...listAnswerStudent,
-        values.message,
-      ]);
-    },
+      newMessageInState(values.message);
+    }
   });
 
-  const loadQuestion = () => {
-    setQuestionCounter(questionCounter + 1);
-    axios.get(`http://10.0.2.2:3000/questions/${questionCounter}`)
-      .then(question => {
-        setQuestionList((listQuestion) => [
-          ...listQuestion,
-          question.data,
-        ]);
-      })
-      .catch(err => console.log(`Error to retrieve question list ${err}`));
-  }
+  useEffect(() => {
+    console.log('ID:', countMessageUser)
+  }, [])
+
+  const newMessageInState = (inputText: string) => {
+    let newMessage = { id: countMessageUser, response: inputText, title: '' };
+    setMessage((previousMessage) => [...previousMessage, newMessage]);
+    if (countMessageUser < 4) {
+      axios.get(`http://192.168.0.251:3000/questions/${countMessageUser}`).then(response => {
+        setTimeout(() => {
+          return setMessage((previousMessage) => [...previousMessage, response.data]);
+        }, 1000);
+      }).catch(err => {
+        console.log('Não foi possível carregar as perguntas');
+        return err;
+      });
+    } else {
+      setMessage((previousMessage) => [...previousMessage, { id: 99, title: 'Parabéns' }]);
+
+    }
+  };
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <Container style={{ backgroundColor: colors.white }}>
         <ScrollView>
           {
-            listQuestion.length !== 0 ? listQuestion.map((question: IPayloadQuestions) => <BallonQuestion key={question.id} title={question.title} />) : <></>
-          }
-          {
-            listAnswerStudent.length !== 0 ? listAnswerStudent.map((answer, i) => <BalloonAnswer key={i} title={answer} />) : <></>
+            message.map((question: IPayloadQuestions, index) => {
+              return (
+                <>
+                  {question.title !== '' ? <BallonQuestion key={index * 2} title={question.title} /> : null}
+                  {question.response !== '' && question.response ? <BalloonAnswer key={countMessageUser + 1} title={question.response} /> : null}
 
+                </>
+              )
+            })
           }
+
         </ScrollView>
 
-        <TouchableOpacity
-          onPress={() => {
-            loadQuestion();
-            formik.handleSubmit();
-          }}
-        >
-          <View
-            style={{
-              backgroundColor: colors.blueSecondary,
-              width: 45,
-              height: 45,
-              borderRadius: 100,
-              justifyContent: 'center',
-              
-            }}
-          >
-            <Text>
-              send
-            </Text>
-          </View>
-        </TouchableOpacity>
 
-        <FormikProvider value={formik}>
-          <TextInput
-            value={formik.values.message}
-            onChangeText={formik.handleChange('message')}
-            returnKeyType="next"
-            style={{
-              backgroundColor: colors.grayLight,
-              borderColor: colors.black,
-              borderWidth: 1,
-              borderRadius: 8,
-              paddingLeft: 15,
-              paddingRight: 15,
-              width: `90%`
-            }}
-            placeholder="Digite aqui sua mensagem"
-          />
-          {formik.touched.message && formik.errors.message && (
-            <Text>{formik.errors.message}</Text>
-          )}
-
-        </FormikProvider>
+          {
+            countMessageUser < 4 
+            ? <FormikProvider value={formik}>
+            <TouchableOpacity
+              onPress={() => {
+                setCounterMessageUser(countMessageUser + 1);
+                formik.handleSubmit();
+              }}
+            >
+  
+              <View
+                style={{
+                  backgroundColor: colors.blueSecondary,
+                  width: 45,
+                  height: 45,
+                  borderRadius: 100,
+                  justifyContent: 'center',
+  
+                }}
+              >
+                <Text>
+                  send
+                </Text>
+              </View>
+            </TouchableOpacity>
+            <TextInput
+              value={formik.values.message}
+              onChangeText={formik.handleChange('message')}
+              returnKeyType="next"
+              style={{
+                backgroundColor: colors.grayLight,
+                borderColor: colors.black,
+                borderWidth: 1,
+                borderRadius: 8,
+                paddingLeft: 15,
+                paddingRight: 15,
+                width: `90%`
+              }}
+              placeholder="Digite aqui sua mensagem"
+            />
+            {formik.touched.message && formik.errors.message && (
+              <Text>{formik.errors.message}</Text>
+            )}
+  
+          </FormikProvider> : <View><Text>Próxima lição</Text></View>
+          }
+        
       </Container>
+
     </SafeAreaView>
 
   );
